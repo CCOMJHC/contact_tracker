1d85b6018b2a50a5c8665ba6ed7be7480556033f
Rachel Cates <rc1104@wildcats.unh.edu>
2020-01-06 07:19:21 -0500

    1) Added in the code to remove contacts that haven't been detected recently. 2) Added variables that get assigned the Detect message's fields. 3) Gave function more appropriate name. 4) Lookup in the dictionary done with hypothetical id rather than a timestamp. 5) moved update and predict outside of the if else condition.

--- contact_tracker/nodes/tracker.py
+++ contact_tracker/nodes/tracker.py
@@ -7,16 +7,16 @@
 import math
 import time
 import rospy
-import hashlib
 import datetime
 
 import kalman_tracker.contact
+from marine_msgs.msgs import Detect
 
 from filterpy.kalman import KalmanFilter
 from filterpy.kalman import update
 from filterpy.kalman import predict
 
-
+MAX_TIME = 50.00
 
 class KalmanTracker:
    """ 
@@ -31,7 +31,7 @@ class KalmanTracker:
        self.all_contacts = {}
 
 
-    def detect_is_contact(all_contacts, hash_key):
+    def detect_not_already_contact(all_contacts, hash_key):
         """
         Returns true if this contact is already in the dictionary, 
         false otherwise.
@@ -49,35 +49,45 @@ class KalmanTracker:
         """
        
         # Get necessary info from the Detect data
-        x0 = 
-        timestamp = datetime.now()
+        header = data.header
+        seq = data.header.seq
+        stamp = data.header.stamp
+        frame_id = data.header.frame_id
+        x_pos = data.pose.pose.position.x
+        y_pos = data.pose.pose.position.y
+        z_pos = data.pose.pose.position.z
+
+        # TODO: Figure out how to determine whether two contacts are the same
+        # (I think they each have a unique ID, but I need to verify this).
+        #contact_id = ???
+        timestamp = datetime.datetime.now().timestamp()
         
-        # Generate new hash key
-        hash_str = str(datetime.datetime.now().timestamp())
-        hash_object = hashlib.md5(hash_str.encode())
-        hash_key = hash_object.hexdigest()
-
-        if detect_is_contact(self.all_contacts, hash_key): 
+        if detect_not_already_contact(self.all_contacts, contact_id): 
             # Create new contact object
-            kf = KalmanFilter(dim_x=6, dim_z=6)
-            c = Contact(x0, kf, timestamp, hash_key)  
+            kf = KalmanFilter(dim_x=6, dim_z=6) # TODO: Are these the correct dimensions?
 
-            # Integrate this with self.kalman_filter 
-            c.kf.predict()
-            c.kf.update(c.z)
+            # TODO: Figure out how to use the detect info to initialize the Kalman
+            # filter for this Contact object
+            c = Contact(header, kf, timestamp, contact_id)  
 
-            # Also add to all_contacts
-            self.all_contacts[hash_key] = c
+            # Add this new object to all_contacts
+            self.all_contacts[contact_id] = c
          
         else:
-            # update the time stamp for when this contact was last accessed so we know not
+            # Update the time stamp for when this contact was last accessed so we know not
             # to remove it anytime soon.
-            c = all_contacts[hash_key]
-            c.last_accessed = datetime.now()
+            c = all_contacts[contact_id]
+            c.last_accessed = timestamp 
 
-            # TODO: Figure out if I need to call predict and update here too
+        # Integrate this with self.kalman_filter 
+        c.kf.predict()
+        c.kf.update(c.z)
 
-        # TODO: remove items from the dictionary that have not been accessed in a while    
+        # Remove items from the dictionary that have not been accessed in a while    
+        # TODO: make this more efficient
+        for item in self.all_contacts:
+            if timestamp - item.last_accessed > MAX_TIME:
+                del all_contacts[item]
 
 
     def run(self):
@@ -86,14 +96,16 @@ class KalmanTracker:
         """
 
         rospy.init_node('tracker', anonymous=True)
-        rospy.Subscriber('/detects', Detect, self.callback) # TODO: Not sure about the 2nd param
+        rospy.Subscriber('/detects', Detect, self.callback) 
         rospy.spin()
 
 
 if __name__=='__main__':
+    
     try:
         kt = KalmanTracker()
         kt.run()
+    
     except rospy.ROSInterruptException:
         print('Falied to initialize KalmanTracker')
         pass
